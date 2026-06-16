9fab91e0d3c1eab854b1c24a72d8e5615ba4e302
khushi8825 <khushisingh051211@gmail.com>
2026-06-17 00:47:47 +0530

    Set up

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
-function Dashboard(){
-    return (
-        <div>Dashboard</div>
+import React from 'react';
+const Dashboard = ()=>{
+    return(
+        <div>Dashboard Page</div>
     )
 }
 export default Dashboard;
